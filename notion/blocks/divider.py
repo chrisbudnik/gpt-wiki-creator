021efaf44e4e69150f7f42aded488a77f3eb8008
chrisb <chrism.budnik@gmail.com>
2023-10-02 20:15:01 +0200

    update: divider init

--- notion/blocks/divider.py
+++ notion/blocks/divider.py
@@ -1,6 +1,11 @@
 from block import NotionBlock
 
 
-
 class Divider(NotionBlock):
-    pass
+    def __init__(self) -> None:
+        super().__init__(type="divider")
+
+    def to_dict(self):
+        return {
+            
+        }
