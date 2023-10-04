55bfad62e54ff76a1386d1b26ad3e723ae3415bf
chrisb <chrism.budnik@gmail.com>
2023-10-04 19:35:59 +0200

    add: prompt schema

--- notion/blockinstruction.py
+++ notion/blockinstruction.py
@@ -1,8 +1,13 @@
 
+# prompt
+prompt = """become a markdown converter. the convertion looks like this: translate md formatting 
+into list of python dicts in this format {name: name_of_formatting, text: value}. 
+this means when provided a md header like this: ### Heading 3 the answer shoud be [{"name": "H3", "test": "Heading 3"}] """
+
 
 class BlockInstrictions:
     """Converts markdown to Notion block instructions"""
-    
+
     def __init__(self, markdown: str):
         self.markdown = markdown
         self.instruction = {}
